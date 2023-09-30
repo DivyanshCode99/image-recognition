0ea71f7da5b2eeacb6e45a1758b9aaf61cd416d4
DivyanshCode99 <131375172+DivyanshCode99@users.noreply.github.com>
2023-09-30 18:46:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: "png",
+    png_quality:90
+})
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+    })
+}
+console.log("Ml5 version:"+ ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/asQG-1CVy/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("model is loaded");
+    
+}
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,getResult);
+}
+function getResult(error,results){
+if(error){
+    console.error(error);
+}
+else{
+console.log(results);
+document.getElementById('result_object_name').innerHTML=results[0].label;
+accurancy=results[0].confidence*100;
+document.getElementById('result_object_accurancy').innerHTML=accurancy.toFixed(2)+"%";
+
+}
+}
